0bccd1908a0d2eaef213b64473035947dc1925c2
Rex <tsunejui@gmail.com>
2022-08-28 03:06:07 +0800

    feat(NavBar.tsx): add react-icons package

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,13 @@ import { FC } from "react";
 
 const NavBar: FC = () => {
     return (
-        <>
-        </>
+        <nav className="w-64 bg-gray-900 min-h-screen pt-20 fixed top-0 left-0">
+            <ul className="text-white">
+                <li className="p-3 hover:bg-gray-700 cursor-pointer">Home</li>
+                <li className="p-3 hover:bg-gray-700 cursor-pointer">About</li>
+                <li className="p-3 hover:bg-gray-700 cursor-pointer">Contact</li>
+            </ul>
+        </nav>
     )
 }
 
